4596acb6ba747315bb8f0d81467a0f1175d800ec
HafsaZareen <hafsazareen064@gmail.com>
2025-03-09 09:15:04 +0000

    kjh

--- Week4/Day5/file_handling.py
+++ Week4/Day5/file_handling.py
@@ -2,33 +2,33 @@
 import os
 #1. Create a file
 def create_file():
-    with open("/mnt/d/mthree/notes/week4/Day5", "w") as file:
+    with open("/mnt/d/mthree/notes/week4/Day5/test.txt", "w") as file:
         file.write("Hello, World!")
 
 
 #2. Write to a file
 def write_to_file():
-    with open("/mnt/d/mthree/notes/week4/Day5", "w") as file:
+    with open("/mnt/d/mthree/notes/week4/Day5/test.txt", "w") as file:
         file.write("Hello, World!")
 
 #3. Read from a file
 def read_from_file():
     print(os.getcwd())
-    with open("/mnt/d/mthree/notes/week4/Day5", "r") as file:
+    with open("/mnt/d/mthree/notes/week4/Day5/test.txt", "r") as file:
         print(file.read())
 
 #4. Append to a file
 def append_to_file():
-    with open("/mnt/d/mthree/notes/week4/Day5", "a") as file:
+    with open("/mnt/d/mthree/notes/week4/Day5/test.txt", "a") as file:
         file.write("Hello, World!")
 
 #5. Delete a file   
 def delete_file():
-    os.remove("/mnt/d/mthree/notes/week4/Day5")
+    os.remove("/mnt/d/mthree/notes/week4/Day5/test.txt")
 
 #6. Check if a file exists
 def check_if_file_exists():
-    if os.path.exists("/mnt/d/mthree/notes/week4/Day5"):
+    if os.path.exists("/mnt/d/mthree/notes/week4/Day5/test.txt"):
         print("File exists")
     else:
         print("File does not exist")
@@ -44,31 +44,31 @@ def list_files_in_directory():
 
 #9. Get the size of a file
 def get_size_of_file():
-    print(os.path.getsize("/mnt/d/mthree/notes/week4/Day5"))
+    print(os.path.getsize("/mnt/d/mthree/notes/week4/Day5/test.txt"))
 
 #10. Get the last modified time of a file
 def get_last_modified_time_of_file():
-    print(os.path.getmtime("/mnt/d/mthree/notes/week4/Day5"))
+    print(os.path.getmtime("/mnt/d/mthree/notes/week4/Day5/test.txt"))
 
 #11. Get the file type
 def get_file_type():
-    print(os.path.splitext("/mnt/d/mthree/notes/week4/Day5"))
+    print(os.path.splitext("/mnt/d/mthree/notes/week4/Day5/test.txt"))
 
 #12. Get the file name
 def get_file_name():
-    print(os.path.basename("/mnt/d/mthree/notes/week4/Day5"))
+    print(os.path.basename("/mnt/d/mthree/notes/week4/Day5/test.txt"))
 
 #13. Get the file extension
 def get_file_extension():
-    print(os.path.splitext("/mnt/d/mthree/notes/week4/Day5"))[1]
+    print(os.path.splitext("/mnt/d/mthree/notes/week4/Day5/test.txt")[1])
 
 #14. Get the file path
 def get_file_path():
-    print(os.path.abspath("/mnt/d/mthree/notes/week4/Day5"))
+    print(os.path.abspath("/mnt/d/mthree/notes/week4/Day5/test.txt"))
 
 #15. Get the file directory
 def get_file_directory():
-    print(os.path.dirname("/mnt/d/mthree/notes/week4/day4"))
+    print(os.path.dirname("/mnt/d/mthree/notes/week4/Day5/test.txt"))
 
 if __name__ == "__main__":
     
